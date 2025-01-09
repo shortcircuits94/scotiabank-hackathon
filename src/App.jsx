298e7681ae66b7a229ec5a6ce7a89cc73f52b916
vanessa-tm <vanessatmiller@gmail.com>
2025-01-09 15:12:19 -0700

    build pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Details from "./pages/Details/Details.jsx";
 
 import "./App.scss";
 
 function App() {
-  return <></>;
+  return 
+  <>
+    <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/:id" element={<Details />} />
+        </Routes>
+      </BrowserRouter>
+  </>;
 }
 
 export default App;
