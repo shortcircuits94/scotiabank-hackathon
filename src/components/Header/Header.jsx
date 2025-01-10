b00c0fceae1d669854f4cbf0f9e0af3a347d82c6
Vanessa Tseng <vanessatsengyh@gmail.com>
2025-01-10 01:40:25 -0500

    linked header home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import "./Header.scss";
 import logo from "../../assets/logo/Scotiabank_logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="header">
-      <img className="header__logo" src={logo} />
-      <h3 className="header__login">Login</h3>
-    </header>
+    <Link to="/">
+      <header className="header">
+        <img className="header__logo" src={logo} />
+        <h3 className="header__login">Login</h3>
+      </header>
+    </Link>
   );
 }
 
