5b4c806ebd6e62073d4490596f0988af793b8471
Vanessa Tseng <vanessatsengyh@gmail.com>
2025-01-10 13:21:48 -0500

    book now changes

--- src/components/BottomDetails/BottomDetails.jsx
+++ src/components/BottomDetails/BottomDetails.jsx
@@ -2,61 +2,54 @@ import React, { useState, useEffect } from "react";
 import data from "../../data/advisors.json";
 import "./BottomDetails.scss";
 
+function BottomDetails({ id }) {
+  const [overview, setOverview] = useState("");
 
-function BottomDetails ( { id }) {
-
-    const [overview, setOverview] = useState("");
-
-
-    useEffect(() => {
+  useEffect(() => {
     const selected = data.find((item) => item.id === id);
     setOverview(selected ? selected["detailed-overview"] : "Not found");
   }, [id]);
 
+  return (
+    <>
+      <section className="bottom-details">
+        <div className="bottom-details__parent">
+          <p className="bottom-details__title">Overview</p>
+          <p className="bottom details__text">{overview}</p>
+        </div>
 
-    return (
-        <>
-            <section className="bottom-details">
-                <div className="bottom-details__parent">
-                    <p className="bottom-details__title">Overview</p>
-                    <p className="bottom details__text">{overview}</p>
-                </div>
-
-                <div className="bottom-details__container">
-                    <div className="bottom-details__bookings">
-                        <div>
-                            <p className="bottom-details__title">General Chat Session</p>
-                            <p className="bottom-details__info">30 minutes</p>
-                        </div>
-                        <div className="bottom-details__container-button">
-                            <button className="bottom-details__button">Book Now</button>
-                        </div>
-                    </div>
-                  <div className="bottom-details__bookings">
-                        <div>
-                            <p className="bottom-details__title">General Chat Session</p>
-                            <p className="bottom-details__info">30 minutes</p>
-                        </div>
-                        <div className="bottom-details__container-button">
-                            <button className="bottom-details__button">Book Now</button>
-                        </div>
-                    </div>
-                   <div className="bottom-details__bookings">
-                        <div>
-                            <p className="bottom-details__title">General Chat Session</p>
-                            <p className="bottom-details__info">30 minutes</p>
-                        </div>
-                        <div className="bottom-details__container-button">
-                            <button className="bottom-details__button">Book Now</button>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            
-        </>
-       
-
-    );
+        <div className="bottom-details__container">
+          <div className="bottom-details__bookings">
+            <div>
+              <p className="bottom-details__title">General Chat Session</p>
+              <p className="bottom-details__info">30 minutes</p>
+            </div>
+            <div className="bottom-details__container-button">
+              <button className="bottom-details__button">Book Now</button>
+            </div>
+          </div>
+          <div className="bottom-details__bookings">
+            <div>
+              <p className="bottom-details__title">Strategic Planning</p>
+              <p className="bottom-details__info">45 minutes</p>
+            </div>
+            <div className="bottom-details__container-button">
+              <button className="bottom-details__button">Book Now</button>
+            </div>
+          </div>
+          <div className="bottom-details__bookings">
+            <div>
+              <p className="bottom-details__title">Comprehensive Review</p>
+              <p className="bottom-details__info">1 hour</p>
+            </div>
+            <div className="bottom-details__container-button">
+              <button className="bottom-details__button">Book Now</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default BottomDetails;
